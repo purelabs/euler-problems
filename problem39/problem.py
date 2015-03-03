e4e8f6b3cfd4dbacb8455d0005544eb06dc51218
Ruben Grill <ruben.grill@gmail.com>
2015-03-03 20:44:43 +0100

    Works

--- problem39/problem.py
+++ problem39/problem.py
@@ -1,24 +1,30 @@
-def concatenate_product(x, y, length=None):
-    result = ''
-    for n in range(1, y + 1):
+def concatenate_pandigital_product(x, y, length=None):
+    value = ''
+    contained_digits = set()
+    result = (None, None)
+
+    for n in range(1, y+1):
         p = str(x*n)
-        if length and len(result) + len(p) > length:
+        if (length and len(value) + len(p) > length) or ('0' in p):
             n -= 1
             break
-        result += p
+        value += p
+        value_int = int(value)
+        contained_digits.update(p)
 
-    return (n, int(result))
+        if len(contained_digits) == 9 and (not result[1] or value_int > result[1]):
+            result = (n, value_int)
+
+    return result
 
 
-def solution(length):
-    max_y = 0
-    max_value = 0
+def solution():
+    max_x, max_y, max_value = None, None, None
 
-    for i in range(1, 10**length):
-        y, value = concatenate_product(i, length, length)
+    for x in range(1, 10000):
+        y, value = concatenate_pandigital_product(x, 9, 9)
 
-        if value > max_value:
-            max_value = value
-            max_y = y
+        if y > 1 and value > max_value:
+            max_x, max_y, max_value = x, y, value
 
-    return (max_y, max_value)
+    return (max_x, max_y, max_value)
--- problem39/test.py
+++ problem39/test.py
@@ -1,19 +1,28 @@
-from problem import concatenate_product, solution
+from problem import concatenate_pandigital_product, solution
 
-def test_192_n_1():
-    assert concatenate_product(192, 1) == (1, 192)
+def test_192_n_2():
+    assert concatenate_pandigital_product(192, 2) == (None, None)
 
 def test_192_n_3():
-    assert concatenate_product(192, 3) == (3, 192384576)
+    assert concatenate_pandigital_product(192, 3) == (3, 192384576)
 
-def test_192_n_5():
-    assert concatenate_product(9, 5) == (5, 918273645)
+def test_192_n_4():
+    assert concatenate_pandigital_product(192, 4) == (4, 192384576768)
 
-def test_192_n_2_length_3():
-    assert concatenate_product(192, 2, 3) == (1, 192)
+def test_192_n_2_length_9():
+    assert concatenate_pandigital_product(192, 2, 9) == (None, None)
 
-def test_solution_1():
-    assert solution(1) == (1, 9)
+def test_192_n_3_length_9():
+    assert concatenate_pandigital_product(192, 3, 9) == (3, 192384576)
 
-def test_solution_2():
-    assert solution(2) == (2, 99)
+def test_192_n_4_length_9():
+    assert concatenate_pandigital_product(192, 4, 9) == (3, 192384576)
+
+def test_192_n_4_length_12():
+    assert concatenate_pandigital_product(192, 4, 12) == (4, 192384576768)
+
+def test_987654321_n_1_length_9():
+    assert concatenate_pandigital_product(987654321, 1, 9) == (1, 987654321)
+
+def test_solution():
+    assert solution() == (9327, 2, 932718654)
